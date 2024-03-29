89b9905e29282d0eb89f2a2f6ce85a7e0646a0ac
AKSHAT MITTAL <72292446+AKSHAT-MITTAL22@users.noreply.github.com>
2024-03-29 18:16:22 +0000

    datatypes

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,20 @@
+"use strict;" //treaat all JS code as newer version
+
+//alert(3+3) //we are using nodejs, not the browser
+
+let name = "akshat"
+let age = 21
+let isLoggedIn = false
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null > standalone value
+// undefined
+// symbol => uniqueness
+
+// object
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
